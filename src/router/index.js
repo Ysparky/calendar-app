cea0abbe0fe984886c0558e39fbed6f58740f64c
José Caldas <jscr272000.jscr@gmail.com>
2021-03-01 04:57:04 -0500

    Feat: Router and Store

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,22 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import "fullcalendar/dist/fullcalendar.css";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
+    path: "/",
+    name: "Home",
     component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
-]
+];
 
 const router = new VueRouter({
+  base: process.env.BASE_URL,
+  mode: "history",
   routes
-})
+});
 
-export default router
+export default router;
